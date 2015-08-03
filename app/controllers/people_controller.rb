d7ecba24901ee5115539fe1e90a10642f4136276
Frankie Roberto <frankie@frankieroberto.com>
2015-08-03 22:25:40 +0100

    person page

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -10,6 +10,29 @@ class PeopleController < ApplicationController
 
     @person = Person.find(id)
 
+    thing_ids = @person.records.pluck(:id)
+
+    @co_authors = Creator
+      .joins(:person)
+      .select("people.*, count(creators.id) as count")
+      .where(record_id: thing_ids)
+      .where.not(person_id: @person.id)
+      .group('people.id')
+      .order('count desc')
+      .limit(10)
+
+    if @person.born_in
+
+      born_in_range = (@person.born_in - 4)..(@person.born_in + 4)
+
+      @contemporaries = Person
+        .where(born_in: born_in_range)
+        .where.not(id: @person.id)
+        .order('records_count desc')
+        .limit(10)
+
+    end
+
     @things = @person.records.select(:identifier, :title).limit(100)
   end
 
