897d60ca9a0732ffb52028ce88038e8d2e776a3c
Frankie Roberto <frankie@frankieroberto.com>
2015-08-03 14:46:09 +0100

    Cache for 10 seconds
    Let’s keep it short for now.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter do
+    set_cache_header(10)  # 20 seconds
+  end
+
+  private
+
+  def set_cache_header(seconds)
+    unless Rails.env.to_s == 'development'
+      response.headers = {"Cache-Control" => "public, max-age=#{seconds}"}
+    end
+  end
+
 end
