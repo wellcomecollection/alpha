fa970a1e1925b81c4331645dfc0d58f70684b90b
Frankie Roberto <frankie@frankieroberto.com>
2015-08-03 09:49:37 +0100

    Week 4 stub

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.2.3'
+gem 'pg'
+gem 'rails_12factor'
+gem 'puma'
+
+group :development, :test do
+  gem 'byebug'
+  gem 'web-console', '~> 2.0'
+  gem 'spring'
+end
+
---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+#require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module WellcomeWeek4Scalability
+  class Application < Rails::Application
+
+    config.session_store :disabled
+
+    config.serve_static_files = true
+
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
