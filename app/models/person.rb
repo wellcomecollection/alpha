4496909b887b2d9358c2934388424e70ba370d01
Frankie Roberto <frankie@frankieroberto.com>
2015-08-04 12:21:21 +0100

    rescue from 404s

--- app/models/person.rb
+++ app/models/person.rb
@@ -25,26 +25,31 @@ class Person < ActiveRecord::Base
 
       url = URI.escape("http://viaf.org/viaf/sourceID/LC%7C#{identifiers['loc']}/justlinks.json")
 
-      open(url) do |file|
+      begin
+        open(url) do |file|
 
-        viaf_concordances = JSON.parse(file.read)
+          viaf_concordances = JSON.parse(file.read)
 
-        identifiers['viaf'] = viaf_concordances["viafID"]
+          identifiers['viaf'] = viaf_concordances["viafID"]
 
-        viaf_concordances.except("viafID", "LC", "Wikipedia").each_pair do |key, value|
-          identifiers[key.downcase] ||= value.to_a.first
+          viaf_concordances.except("viafID", "LC", "Wikipedia").each_pair do |key, value|
+            identifiers[key.downcase] ||= value.to_a.first
+          end
+
+          viaf_concordances["Wikipedia"].to_a.each do |wikipedia_url|
+
+            wikipedia_language = wikipedia_url[wikipedia_url_regxex, 1]
+            wikipedia_slug = wikipedia_url[wikipedia_url_regxex, 2]
+
+            identifiers["wikipedia_#{wikipedia_language}"] ||= wikipedia_slug
+
+          end
+
+          save!
+
+        rescue OpenURI::HTTPError
+          puts "Error: #{url}"
         end
-
-        viaf_concordances["Wikipedia"].to_a.each do |wikipedia_url|
-
-          wikipedia_language = wikipedia_url[wikipedia_url_regxex, 1]
-          wikipedia_slug = wikipedia_url[wikipedia_url_regxex, 2]
-
-          identifiers["wikipedia_#{wikipedia_language}"] ||= wikipedia_slug
-
-        end
-
-        save!
 
       end
 
