344194c51f6ca1eccc098766bffa47c70685bd90
Frankie Roberto <frankie@frankieroberto.com>
2015-12-16 16:06:38 +0000

    Allow assets to be cached for a year

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,7 @@ Rails.application.configure do
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
+  config.static_cache_control = "public, max-age=31536000"
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
