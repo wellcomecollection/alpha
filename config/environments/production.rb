1b85ee4dccd74d43be46f893872ce5429284a0fa
Frankie Roberto <frankie@frankieroberto.com>
2015-12-17 17:05:39 +0000

    set url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  Rails.application.routes.default_url_options[:host] = ENV.fetch('HOST')
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
