4e5bad14c9deda6ccddb1c9195172cec07d5a72c
George Oates <glo@Georges-MacBook-Pro.local>
2016-01-12 17:19:27 +0000

    showing less subjects

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -56,7 +56,7 @@ class PeopleController < ApplicationController
       .where(["taggings.record_id IN (select creators.record_id from creators where creators.person_id = ?)", @person.id])
       .group('subjects.id')
       .order('count desc')
-      .limit(100)
+      .limit(40)
 
     @things = @person.records.select(:identifier, :title, :pdf_thumbnail_url, :cover_image_uris)
       .order('digitized desc')
