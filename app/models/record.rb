f037af7da3c51e55d3014403b1dd624c6f8548b9
Tom Stuart <tom@codon.com>
2016-01-13 16:29:11 +0000

    Store subject IDs in Elasticsearch record index
    We’re doing this so that we can aggregate the record counts by year in
    order to draw the publication graph.

--- app/models/record.rb
+++ app/models/record.rb
@@ -34,7 +34,8 @@ class Record < ActiveRecord::Base
       year: year,
       pdf_thumbnail_url: pdf_thumbnail_url,
       cover_image_uris: cover_image_uris,
-      archives_ref: archives_ref
+      archives_ref: archives_ref,
+      subject_ids: taggings.pluck(:subject_id)
     }
 
   end
