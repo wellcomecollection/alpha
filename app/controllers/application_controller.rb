c2cbcbb5a34ac89215ce3246b7e332a9d8273480
Frankie Roberto <frankie@frankieroberto.com>
2016-01-13 20:29:25 +0000

    Turn off sessions, use 30 cookie for sign in
    This would enable us to have CloudFront cache the same HTML page for
    all logged out users (were we to set a suitable Expires or
    Cache-Control header).
    
    Downside: No cross-site request forgery protection. Perhaps we could
    selectively turn this back on for logged-in users only?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
 
   # before_filter do
   #   set_cache_header(60 * 60 * 24 * 7)  # 7 days
   # end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(cookies.encrypted[:user_id]) if cookies.encrypted[:user_id]
   end
 
   def logged_in_as_admin?
