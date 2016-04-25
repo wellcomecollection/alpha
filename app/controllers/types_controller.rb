ca52148aaec697dd545d898f75c79e76c5c0a206
Frankie Roberto <frankie@frankieroberto.com>
2016-04-25 20:43:51 +0100

    Thumbnails

--- app/controllers/types_controller.rb
+++ app/controllers/types_controller.rb
@@ -7,7 +7,8 @@ class TypesController < ApplicationController
   def show
     @type = Type.find(params[:id].gsub('T',''))
 
-    @records = @type.records.select(:identifier, :title).limit(100)
+    @records = @type.records.select(:identifier, :title, :pdf_thumbnail_url,
+      :cover_image_uris).order('digitized desc', :title).limit(100)
   end
 
 end
