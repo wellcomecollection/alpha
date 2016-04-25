f3f6d788ab45416fa801be5a6c2b2d5a1486d12e
Frankie Roberto <frankie@frankieroberto.com>
2016-04-25 15:57:35 +0100

    Whitelist the codes

--- app/jobs/add_to_collection_from_dig_code_job.rb
+++ app/jobs/add_to_collection_from_dig_code_job.rb
@@ -3,14 +3,39 @@ class AddToCollectionFromDigCodeJob < ActiveJob::Base
 
   def perform(record)
 
-    local_codes = record.metadata.fetch('759', []).map {|field| field['a'].to_s }
+    DIG_CODES = {
+      'diggentics' => 'Genetics',
+      'digukmhl' => '',
+      'digmhl(nlm)' => '',
+      'digrecipe' => 'Recipes',
+      'digtdnet' => '',
+      'digmoh' => 'Ministry of Health reports',
+      'digmhl(francis)' => '',
+      'digmhl(cushing)' => '',
+      'dig19th' => '',
+      'digglasgow' => '',
+      'digaids' => '',
+      'digmhl(columbia)' => '',
+      'digmhl(gerstein)' => '',
+      'digucl(ophta)' => '',
+      'digramc' => '',
+      'diglshtm' => '',
+      'digfilm' => '',
+      'digarabic' => '',
+      'digasylum' => '',
+      'digrcpe' => '',
+      'digthomson' => '',
+      'digleeds' => '',
+    }
+
+    local_codes = record.metadata.fetch('759', []).map {|field| field['a'].to_s.strip }
 
     local_codes.each do |local_code|
 
-      if local_code =~ /\Adig/
+      if DIG_CODES[local_code]
 
         collection = Collection.find_by_dig_code(local_code) ||
-          Collection.create!(dig_code: local_code, name: local_code)
+          Collection.create!(dig_code: local_code, name: DIG_CODES[local_code])
 
         begin
           record.collections << collection
---
+++ lib/tasks/collections.rake
@@ -0,0 +1,25 @@
+namespace :ingest do
+
+  desc 'Import collections from DIG codes'
+  task queue_all_for_import_from_dig_code: :environment do
+
+    time = Time.now
+
+    Record.select(:id)
+      .find_in_batches(batch_size: 500)
+      .with_index do |batch, batch_number|
+
+      print "Processing batch #{batch_number + 1}... "
+
+      batch.each do |record|
+        AddToCollectionFromDigCodeJob.perform_later(record)
+      end
+
+      puts "Done in #{Time.now - time} seconds"
+      time = Time.now
+
+    end
+
+  end
+
+end
