a79744719e321c7f0c115dbdcbf556f1c0834a3a
Frankie Roberto <frankie@frankieroberto.com>
2016-04-25 20:32:09 +0100

    method for update digitised count

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -3,4 +3,8 @@ class Collection < ActiveRecord::Base
   has_many :collection_memberships, dependent: :destroy
   has_many :records, through: :collection_memberships
 
+  def update_digitized_records_count!
+    update_attribute(:digitized_records_count, records.digitized.count)
+  end
+
 end
