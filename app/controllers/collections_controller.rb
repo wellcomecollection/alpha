2a01c2031cb944edbec73c3f40db827bb2873336
Frankie Roberto <frankie@frankieroberto.com>
2016-04-26 11:41:08 +0100

    Show top 10 types in collection

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -9,6 +9,16 @@ class CollectionsController < ApplicationController
   def show
     @collection = Collection.find_by_slug!(params[:id])
     @records = @collection.records.order('digitized desc', :title).limit(500)
+
+    @types = RecordType
+      .select('types.*')
+      .select('count(record_types.record_id) as records_in_type_count')
+      .joins(:type, :collection_memberships)
+      .where(collection_memberships: {collection_id: @collection.id})
+      .group('types.id')
+      .order('records_in_type_count desc')
+      .limit(10)
+
   end
 
   def edit
