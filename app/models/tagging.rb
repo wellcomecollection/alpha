df604399675d0e84516a4a2dc4241d5566c2392d
Frankie Roberto <frankie@frankieroberto.com>
2016-04-27 15:31:13 +0100

    association

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -4,6 +4,7 @@ class Tagging < ActiveRecord::Base
   belongs_to :record
 
   has_many :collection_memberships, primary_key: :record_id, foreign_key: :record_id
+  has_many :record_types, primary_key: :record_id, foreign_key: :record_id
 
 
 end
