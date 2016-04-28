dbe60efa7c8842273efba065b06a575b9eb71f17
Frankie Roberto <frankie@frankieroberto.com>
2016-04-28 17:16:52 +0100

    search page fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,30 +2,35 @@ class SearchController < ApplicationController
 
   def show
 
-    @query = params[:q]
-    @per_page = params[:limit].to_i
-    @per_page = 200 unless (1..200).include?(@per_page)
-    @from = params[:from].to_i
+    @query = params[:q].to_s
 
-    client = Elasticsearch::Client.new log: true
+    if !@query.blank?
 
-    results = client.search index: 'records',
-      body: {
-        query: {
-          match: {
-            '_all' => {
-              query: @query,
-              operator: 'and'
+      @per_page = params[:limit].to_i
+      @per_page = 200 unless (1..200).include?(@per_page)
+      @from = params[:from].to_i
+
+      client = Elasticsearch::Client.new log: true
+
+      results = client.search index: 'records',
+        body: {
+          query: {
+            match: {
+              '_all' => {
+                query: @query,
+                operator: 'and'
+              }
             }
-          }
-        },
-        size: @per_page,
-        from: @from
-      }
+          },
+          size: @per_page,
+          from: @from
+        }
 
 
-    @records = results['hits']['hits']
-    @total_count = results['hits']['total']
+      @records = results['hits']['hits']
+      @total_count = results['hits']['total']
+
+    end
 
   end
 
