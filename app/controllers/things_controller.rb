dcde2c8773daa339ac218814b6d84d133dfc0fff
Frankie Roberto <frankie@frankieroberto.com>
2016-04-28 22:11:27 +0100

    'Go deeper'

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -3,6 +3,15 @@ class ThingsController < ApplicationController
   def show
     @thing = Record.find_by!(identifier: params[:id])
     @subjects = @thing.subjects
+
+    if @thing.archives_ref
+
+      @child_records = Record
+        .select(:identifier, :title)
+        .where(parent_id: @thing.id)
+        .order(:title)
+        .limit(500)
+    end
   end
 
 end
