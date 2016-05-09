2e563cd0aefd947a476bed938603dc32eff65b19
Frankie Roberto <frankie@frankieroberto.com>
2016-05-09 16:32:54 +0100

    Counts on newly digitized page

--- app/controllers/newly_digitized_controller.rb
+++ app/controllers/newly_digitized_controller.rb
@@ -1,8 +1,32 @@
 class NewlyDigitizedController < ApplicationController
 
   def show
+
+    @records_digitized_in_past_week_count = Record
+      .where(digitized_at: 7.days.ago..Time.now)
+      .where.not(leader: nil)
+      .count
+
+    @top_subjects = Subject
+      .select('subjects.id, subjects.label, count(taggings.record_id) as records_digitized_recently_count')
+      .joins(taggings: :record)
+      .where(records: {digitized_at: 7.days.ago..Time.now})
+      .order('records_digitized_recently_count desc')
+      .group('subjects.id, subjects.label')
+      .limit(10)
+
+    @top_authors = Person
+      .select('people.id, people.name, count(creators.record_id) as records_digitized_recently_count')
+      .joins(creators: :record)
+      .where(records: {digitized_at: 7.days.ago..Time.now})
+      .order('records_digitized_recently_count desc')
+      .group('people.id, people.name')
+      .limit(10)
+
     @records = Record.select(:id, :identifier, :title, :cover_image_uris, :pdf_thumbnail_url)
       .where.not(digitized_at: nil)
+      .where.not(leader: nil)
+      .where(access_conditions: 'Open')
       .order(:digitized_at).reverse_order
       .limit(200)
   end
