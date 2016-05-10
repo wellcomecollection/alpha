e9a9140e03e0c416ded0ced889d3dd2b6bd72bec
Frankie Roberto <frankie@frankieroberto.com>
2016-05-10 11:37:38 +0100

    Redirect back to page you were on
    Fixes #32

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,16 +9,16 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user.try(:authenticate, params[:password])
       cookies.encrypted[:user_id] = {value: user.id, expires: 30.days.from_now}
-      puts "Saving session as #{user.id}"
-      redirect_to root_path
+
+      redirect_to (params[:return_to] || root_path)
     else
-      redirect_to new_session_path
+      redirect_to new_session_path(return_to: params[:return_to])
     end
   end
 
   def destroy
     cookies.delete(:user_id)
-    redirect_to root_path
+    redirect_to (params[:return_to] || root_path)
   end
 
 end
