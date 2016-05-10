9e6b3d0ce361f7c8e013f2a2574eec4029d9c10b
Frankie Roberto <frankie@frankieroberto.com>
2016-05-10 12:05:19 +0100

    Show values on bar graph
    Fixes #69

--- app/assets/javascripts/publication_graph.js
+++ app/assets/javascripts/publication_graph.js
@@ -1,6 +1,8 @@
 var initPublicationGraph = function () {
   var bars = document.getElementsByClassName('count-bars')[0];
   var labels = document.getElementsByClassName('count-bars-labels')[0];
+  var values = document.getElementsByClassName('count-bars-values')[0];
+
 
   bars.addEventListener('mouseover', function (event) {
     var element = event.target;
@@ -22,6 +24,17 @@ var initPublicationGraph = function () {
 
     var label = labels.children[index];
     label.classList.add('show')
+
+
+    var shownValues = values.getElementsByClassName('show');
+    for (var i = shownValues.length - 1; i >= 0; i--) {
+      shownValues[i].classList.remove('show')
+    };
+
+    var value = values.children[index];
+    value.classList.add('show')
+
+
   });
 
   bars.addEventListener('mouseout', function (event) {
